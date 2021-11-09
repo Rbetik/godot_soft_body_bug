38908d3846471e5cc5e00380d7b4bc28d37c4f70
robert betik <rbetik@seznam.cz>
2021-11-09 12:23:30 +0100

    finished demo for soft body pinned problem

--- active_scene_manager.gd
+++ active_scene_manager.gd
@@ -1,10 +1,11 @@
 extends Node
+# IN demo only one point is pinned, but if you pin another 3 or more, 
+# it also demonstrates problem with more than 3 pinned
+
+#for bug to be reproducable SOFT_BODY_PARENT must have some transform.origin
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var alreadyLoaded = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,3 +15,29 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #    pass
+
+func _unhandled_input(event):
+    if (event.is_action_pressed("change_scene")):
+        self.changeScene()
+        return
+
+func changeScene():
+    # if scene is not yet stored, save it for later
+    if !self.alreadyLoaded.keys().has(self.get_tree().current_scene.name):
+        self.alreadyLoaded[self.get_tree().current_scene.name] = self.get_tree().current_scene
+
+    # toggle switching A goes to B, B goes to A
+    var nextSceneName = 'Scene_A' if self.get_tree().current_scene.name == 'Scene_B' else 'Scene_B'
+
+    #if i dont have scene loaded instance it, else use previously loaded scnene.
+    # THIS IS IMPORTANT FOR BUG, IF YOU LOAD SCNENE FRESH IT WORKS CORRECTLY
+    var nextScene
+    if !self.alreadyLoaded.keys().has(nextSceneName):
+        print('loading from fresh')
+        nextScene = load('res://' + nextSceneName + '.tscn').instance()
+    else:
+        nextScene = alreadyLoaded[nextSceneName]
+    var currentScene = self.get_tree().current_scene
+    self.get_tree().get_root().call_deferred('add_child', nextScene)
+    self.get_tree().call_deferred('set_current_scene', nextScene)
+    self.get_tree().get_root().call_deferred('remove_child', currentScene)
